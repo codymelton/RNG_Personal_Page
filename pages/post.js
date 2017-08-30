9f5e6699676bfe3130bafe55201ab026ac95cdc5
Cody Melton <codymelton@me.com>
2017-08-30 11:24:26 -0600

    Website framework done with basic data handling complete

--- pages/post.js
+++ pages/post.js
@@ -1,14 +1,27 @@
 import Layout from '../components/MyLayout'
+import fetch from 'isomorphic-unfetch'
 
-const Content = (props) => (
-  <div>
+const Post = (props) => (
+  <Layout>
+
+    <h2>{props.show.name}</h2>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+
     <h2> { props.url.query.title } </h2>
     <p>This is the blog post content.</p>
-  </div>
-)
 
-export default (props) => (
-  <Layout>
-    <Content url={ props.url } />
   </Layout>
 )
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const show = await res.json()
+
+  console.log(`Fetched show: ${show.name}`)
+
+  return { show }
+}
+
+export default Post
--- pages/writing.js
+++ pages/writing.js
@@ -1,21 +1,61 @@
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import Markdown from 'react-markdown'
 
-const PostLink = (props) => (
-  <li>
-    <Link as={`/writings`} href={`/post?title=${props.title}`}>
-      <a> {props.title} </a>
-    </Link>
-  </li>
-)
+function getPosts () {
+  return [
+    {id: 'hello-nextjs', title: "Hello Next.js"},
+    {id: 'learn-nextjs', title: "Learn Next = Be Awesome"},
+    {id: 'deploy-nextjs', title: "Can't Wait to Deploy to Zeit"}
+  ]
+}
 
 export default () => (
   <Layout>
     <h2> Writing and Rambling </h2>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next = Be Awesome" />
-      <PostLink title="Can't Wait to Deploy to Zeit" />
+      {getPosts().map((post) =>(
+        <li key={post.id}>
+          <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
+
+    <Markdown source={`
+      This is the markdown blog post.
+      Yes. We can have a [link](/link).
+      And we can have a title as well.
+
+      ### This is a title
+
+      And here's the content.
+    `} />
+
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </Layout>
 )
